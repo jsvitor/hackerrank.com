d699bdbaffeaa46ee37ed1e72daa4727a8ce1b2c
Vitor <jszvitor@gmail.com>
2020-08-03 20:06:20 -0300

    feat(30-of-challenges): solved data types challenge of day 01 with python

---
+++ data-types-py/data-types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = int(input())
+b = float(input())
+# Read and save an integer, double, and String to your variables.
+i += a
+d += b
+s += str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i)
+# Print the sum of the double variables on a new line.
+print(d)
+# Concatenate and print the String variables on a new line
+print(s)
+# The 's' variable above should be printed first.
